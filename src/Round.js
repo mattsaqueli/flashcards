4d52d18d4c86bc0951348b39211f73277c6b3914
Matt Saqueli <msaqueli22@turing.edu>
2023-03-22 19:25:57 -0500

    Complete Round class/test

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,35 @@
+const Turn = require('../src/Turn')
+
 class Round {
   constructor(deck) {
     this.deck = deck
+    this.turnCount = 0
+    this.incorrectAnswer = []
+    this.correctAnswer = []
+  }
+
+  returnCurrentCard() {
+    return this.deck.cards[this.turnCount]
+  }
+
+  takeTurn(guess) {
+    let currentTurn = new Turn(guess, this.returnCurrentCard())
+    this.turnCount++
+    if (guess !== currentTurn.card.correctAnswer) {
+      this.incorrectAnswer.push(currentTurn.card.id)
+    } 
+    else if (guess === currentTurn.card.correctAnswer) {
+      this.correctAnswer.push(currentTurn.card.id)
+    }
+    return currentTurn.giveFeedback()
+  }
+
+  calculatePercentCorrect() {
+   return Math.floor((1 - (this.incorrectAnswer.length / this.turnCount)) * 100)
+  }
+
+  endRound() {
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,9 +1,9 @@
 const chai = require('chai')
 const expect = chai.expect
 
+const Card = require('../src/Card')
 const Round = require('../src/Round')
 const Deck = require('../src/Deck')
-const Card = require('../src/Card')
 
 describe('Round', () => {
   let card1
@@ -12,7 +12,7 @@ describe('Round', () => {
   let deck
   let round
 
-  beforeEach(() => {
+  beforeEach('setup', () => {
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder')
     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald')
@@ -28,7 +28,64 @@ describe('Round', () => {
     expect(round).to.be.an.instanceOf(Round)
   })
 
-  it('should have deck of cards', () => {
+  it('should have a deck of cards', () => {
     expect(round.deck).to.equal(deck)
   })
+
+  it('should have a method that returns the current card', () => {
+    expect(round.returnCurrentCard(), card1)
+  })
+
+  it('should start with 0 turns', () => {
+    expect(round.turnCount).to.equal(0)
+  })
+
+  it('should update turns when a turn a made', () => {
+    expect(round.turnCount, 0)
+    round.takeTurn()
+    expect(round.turnCount, 1)
+    round.takeTurn()
+    expect(round.turnCount, 2)
+  })
+
+  it('should update current card after a turn', () => {
+    round.takeTurn()
+    expect(round.returnCurrentCard(), card2)
+  })
+
+  it('should be able to store correct answers', () => {
+    expect(round.correctAnswer).to.deep.equal([])
+  })
+
+  it('should be able to store incorrect answers', () => {
+    expect(round.incorrectAnswer).to.deep.equal([])
+  })
+
+  it('should give feedback if answer is correct', () => {
+    expect(round.takeTurn('sea otter')).to.deep.equal('correct!')
+  })
+
+  it('should give feedback if answer is incorrect', () => {
+    expect(round.takeTurn('pug')).to.deep.equal('incorrect..')
+  })
+
+  it('should calculate the percentage of correct answers', () => {
+    round.takeTurn('sea otter')
+    expect(round.calculatePercentCorrect()).to.equal(100)
+    round.takeTurn('sea otter')
+    expect(round.calculatePercentCorrect()).to.equal(50)
+    round.takeTurn('appendix')
+    expect(round.calculatePercentCorrect()).to.equal(33)
+  })
+
+
+  it('should have a method that ends the round', () => {
+    round.takeTurn('sea otter')
+    round.takeTurn('appendix')
+    round.takeTurn('William')
+
+
+    expect(round.endRound()).to.equal('** Round over! ** You answered 33% of the questions correctly!')
+  })
+
 })
